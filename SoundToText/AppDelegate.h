c6875d56353f499127354688c016633ea0d22616
Kong Lee <konglee873@gmail.com>
2017-08-21 14:17:26 +0800

    Initial Commit

---
+++ SoundToText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoundToText
+//
+//  Created by kong on 2017/8/21.
+//  Copyright © 2017年 konglee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SoundToText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoundToText
+//
+//  Created by kong on 2017/8/21.
+//  Copyright © 2017年 konglee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
